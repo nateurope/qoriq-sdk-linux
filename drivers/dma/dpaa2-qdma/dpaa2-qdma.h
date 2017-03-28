H-GOTO/linux-v4.1.8-sdk

ls2-qdma: support qDMA from LS2 rev2
Update qDMA driver to work with qDMA IP ver 1.1 from LS2 rev2.
The driver is still compatible with IP v1.0 from rev1.
Set the transaction type for command field for both source and
destination descriptors to coherent copy of cacheable memory.
Don't enable source prefetchable (SPF) in IP v1.0 FD since it
overlaps with IP v1.1 source read transaction QoS field.

Signed-off-by: Catalin Horghidan <catalin.horghidan@nxp.com>

drivers/dma/dpaa2-qdma/dpaa2-qdma.h
@@ -61,7 +61,12 @@ struct dpaa2_qdma_sd_d {
 	uint32_t rbpcmd;	/* Route-by-port command */
 	uint32_t cmd;
 } __attribute__((__packed__));
-#define CMD_TTYPE_RW (0x4 << 28)
+/* Source descriptor command read transaction type for RBP=0:
+ coherent copy of cacheable memory */
+#define QDMA_SD_CMD_RDTTYPE_COHERENT (0b1011 << 28)
+/* Destination descriptor command write transaction type for RBP=0:
+ coherent copy of cacheable memory */
+#define QDMA_DD_CMD_WRTTYPE_COHERENT (0b0110 << 28)
 
 #define QDMA_SG_FMT_SDB		0x0 /* single data buffer */
 #define QDMA_SG_FMT_FDS		0x1 /* frame data section */

nateurope/qoriq-sdk-linux

drivers/dma/dpaa2-qdma/dpaa2-qdma.h
@@ -61,7 +61,12 @@ struct dpaa2_qdma_sd_d {
 	uint32_t rbpcmd;	/* Route-by-port command */
 	uint32_t cmd;
 } __attribute__((__packed__));
-#define CMD_TTYPE_RW (0x4 << 28)
+/* Source descriptor command read transaction type for RBP=0:
+ coherent copy of cacheable memory */
+#define QDMA_SD_CMD_RDTTYPE_COHERENT (0xb << 28)
+/* Destination descriptor command write transaction type for RBP=0:
+ coherent copy of cacheable memory */
+#define QDMA_DD_CMD_WRTTYPE_COHERENT (0x6 << 28)
 
 #define QDMA_SG_FMT_SDB		0x0 /* single data buffer */
 #define QDMA_SG_FMT_FDS		0x1 /* frame data section */